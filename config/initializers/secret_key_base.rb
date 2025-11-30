1a2544c2c7f5d10edf41c7cb9efee6a127ef588b
Joseph Schoblaska <jschoblaska@gmail.com>
2025-11-29 18:43:14 -0600

    set secret key base on config

--- config/initializers/secret_key_base.rb
+++ config/initializers/secret_key_base.rb
@@ -2,4 +2,4 @@
 
 # Allow SECRET_KEY_BASE from environment for containerized deployments
 # without requiring Rails credentials
-Rails.application.secret_key_base = ENV["SECRET_KEY_BASE"] if ENV["SECRET_KEY_BASE"].present?
+Rails.application.config.secret_key_base = ENV["SECRET_KEY_BASE"] if ENV["SECRET_KEY_BASE"].present?
